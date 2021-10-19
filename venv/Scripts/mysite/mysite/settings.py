eb73b572dae4e027272dc14a472d27e4beba1ddb
aravindab201 <aravindreddy201@gmail.com>
2021-10-19 11:47:31 +0530

    latest changes

--- venv/Scripts/mysite/mysite/settings.py
+++ venv/Scripts/mysite/mysite/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+"""STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
@@ -144,5 +144,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS= (
     os.path.join(BASE_DIR,'static'),
 )
-
-MEDIA_URL = '/media/'
+"""
+    #MEDIA_URL = '/media/'
